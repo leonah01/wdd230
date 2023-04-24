f01cb57f97c709bd35809ca7e63d9b1f189af409
leonah01 <don22010@byui.edu>
2023-04-24 19:50:09 +0200

    assi
    Signed-off-by: leonah01 <don22010@byui.edu>

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+function updateTime() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+  
+    document.getElementById('time').innerHTML = 'last updated on:'  +  now 
+  }
+  
+  setInterval(updateTime, 2000);
+  
+const header = document.querySelector('header');
+const footer = document.querySelector('footer');
+
+header.style.textAlign = 'center';
+footer.style.textAlign = 'center';
