630adb42e15d7b07d2c1e6144210a0d754176666
leonah01 <don22010@byui.edu>
2023-07-11 19:15:32 +0200

    TriBak
    Signed-off-by: leonah01 <don22010@byui.edu>

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,12 @@ const options = {
   weekdays: "long",
   day: "numeric",
   month: "long",
-  year: "numeric",
-};
-
-document.getElementById('lastmodified').textContent = new Date().toLocaleDateString('en-US', options)
-const currentYear = new Date().getFullYear();
-document.getElementById('year').textContent = currentYear;
-const currenttime = new TimeRanges().getFullTime;
+    year: "numeric"
+  };
+  
+  document.getElementById('lastmodified').textContent = new Date().toLocaleDateString('en-US', options)
+  const currentYear = new Date().getFullYear();
+  document.getElementById('year').textContent = currentYear;const currenttime = new TimeRanges().getFullTime;
 document.getElementById('time').textContent = currenttime;
   
 const header = document.querySelector('header');
